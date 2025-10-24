3748fbd05743d7075e5afb22df7c55710b7d69e1
qkrtkdgus010208 <psh010208@naver.com>
2025-10-24 19:01:12 +0900

    feat: PlayerController 스크립트 추가

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private static readonly int IsMoving = Animator.StringToHash("IsMove");
+
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rigid;
+    private Animator animator;
+
+    private Vector2 movementDirection;
+    private float speed = 3f;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        rigid.velocity = movementDirection * speed;
+    }
+
+    private void LateUpdate()
+    {
+        animator.SetBool(IsMoving, movementDirection.magnitude > .5f);
+
+        if (movementDirection.x != 0)
+            spriteRenderer.flipX = movementDirection.x < 0;
+    }
+
+    private void OnMove(InputValue inputValue)
+    {
+        movementDirection = inputValue.Get<Vector2>();
+        movementDirection = movementDirection.normalized;
+    }
+}
