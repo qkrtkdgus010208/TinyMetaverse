c83d0adbb977e2643dc112cc95bac152c60e63ed
qkrtkdgus010208 <psh010208@naver.com>
2025-10-24 17:36:05 +0900

    feat: LoadScene 제작

---
+++ Assets/Scripts/Manager/AudioManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/SceneLoader.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public enum SceneNames { StartScene = 0, MainScene, MiniGameScene, LoadScene }
+
+public class SceneLoader : MonoBehaviour
+{
+    static SceneNames nextScene;
+
+    [SerializeField] private Slider slider;
+    [SerializeField] private TextMeshProUGUI text;
+
+    public static void LoadScene(SceneNames sceneName)
+    {
+        nextScene = sceneName;
+        SceneManager.LoadScene(SceneNames.LoadScene.ToString());
+    }
+
+    private void Start()
+    {
+        StartCoroutine(LoadSceneProgress());
+    }
+
+    IEnumerator LoadSceneProgress()
+    {
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(nextScene.ToString());
+        asyncOperation.allowSceneActivation = false;
+
+        float timer = 0f;
+        while (!asyncOperation.isDone)
+        {
+            yield return null;
+
+            if (asyncOperation.progress < 0.9f)
+            {
+                slider.value = asyncOperation.progress;
+                text.text = $"Loading... {(int)(Mathf.Clamp01(asyncOperation.progress / 0.9f) * 100)}%";
+            }
+            else
+            {
+                timer += Time.unscaledDeltaTime;
+                slider.value = Mathf.Lerp(0.9f, 1f, timer);
+
+                if (slider.value >= 1f)
+                {
+                    asyncOperation.allowSceneActivation = true;
+                    yield break;
+                }
+            }
+        }
+    }
+}
