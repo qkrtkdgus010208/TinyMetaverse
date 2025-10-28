4e815ccea16c42cdb3db93c6eed7c4195c378e45
qkrtkdgus010208 <psh010208@naver.com>
2025-10-28 18:02:10 +0900

    feat: Singleton 제네릭 추가

---
+++ Assets/Scripts/Util/Singleton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T Instance { get; private set; }
+
+    protected virtual void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Util/Util.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class Util
+{
+    public static IEnumerator DelayAction(float delay, Action action)
+    {
+        yield return new WaitForSeconds(delay);
+        action?.Invoke();
+    }
+
+    public static IEnumerator DelayActionRealTime(float delay, Action action)
+    {
+        yield return new WaitForSecondsRealtime(delay);
+        action?.Invoke();
+    }
+}
